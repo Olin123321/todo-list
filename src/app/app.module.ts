3e478d2e32f4850c2473e6440b75e4c15f297196
Olin <61127141+Olin123321@users.noreply.github.com>
2022-04-16 16:33:35 +0200

    ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
 import localePl from '@angular/common/locales/pl';
 import { registerLocaleData } from '@angular/common';
 registerLocaleData(localePl);
@@ -12,7 +14,8 @@ registerLocaleData(localePl);
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     { provide: 'LOCALE_ID', useValue: 'pl-PL' }
